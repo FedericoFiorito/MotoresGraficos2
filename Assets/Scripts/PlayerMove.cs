67e1817567b5a449e3132fcd67dc36ee3f8893bf
Federico Fiorito <federicofiorito19@gmail.com>
2022-04-20 12:59:46 -0300

    Mecanica Runner
    TP2: Mecanica Runner

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] LayerMask groundLayers;
+    [SerializeField] private float runSpeed = 50f;
+    [SerializeField] private float jumpHeight = 2f;
+
+    private float gravity = -50f;
+    private CharacterController characterController;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private float horizontalInput;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        horizontalInput = 1;
+
+        transform.forward = new Vector3(horizontalInput, 0, Mathf.Abs(horizontalInput) - 1);
+
+        isGrounded = Physics.CheckSphere(transform.position, 0.1f, groundLayers, QueryTriggerInteraction.Ignore);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        characterController.Move(new Vector3(horizontalInput * runSpeed, 0, 0) * Time.deltaTime);
+
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
